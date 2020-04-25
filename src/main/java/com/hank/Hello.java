83f2017dd68b0a95c7b510fe92fd5312f42ba945
Linyuting <benlin226@gmail.com>
2020-04-25 15:02:05 +0800

    java and kotlin variable, data type

--- src/main/java/com/hank/Hello.java
+++ src/main/java/com/hank/Hello.java
@@ -4,5 +4,9 @@ public class Hello {
     public static void main(String[] args) {
         Person p = new Person();
         p.hello();
+        byte age = 123;
+        short age2 = 123;
+        int age3 = 123;
+        long age4 = 123;
     }
 }
--- src/main/kotlin/com/hank/Hello.kt
+++ src/main/kotlin/com/hank/Hello.kt
@@ -3,6 +3,11 @@ package com.hank
 fun main() {
     var h = Human()
     h.hello()
+    var age = 123
+    val age2 = 123
+
+    var age3 : Int = 123
+    val age4 : Char = '1'
 }
 
 class Human(){
